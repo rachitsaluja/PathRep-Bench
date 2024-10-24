b01a2e502f12b1296d978c45994891efba248ed4
rachitsaluja <salujarachit23@gmail.com>
2024-10-24 11:58:48 -0400

    PathLlama Predictions for Disease

--- job.sh
+++ job.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# For AIC 
+module purge
+module load anaconda3
+
 source activate HF-runner
 
-#cd ./benchmarks/Task1_Disease/
+cd ./benchmarks/Task1_Disease/
 #cd ./benchmarks/Task2_Stage/
 #cd ./extras/
-cd ./benchmarks/Task3_Prognosis/
+#cd ./benchmarks/Task3_Prognosis/
 
 #python preds-llama3-8b.py
 #python preds-llama3-70b.py
@@ -29,3 +33,4 @@ python preds-gpt-4o-mini-FT.py
 #python preds-mistral-medium.py
 #python preds-mistral-large.py
 
+python preds-pathllama3.1-8b.py
---
+++ submit_job_GPU.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p job_err_EXP
+mkdir -p job_out_EXP
+
+sbatch --requeue \
+        -p sablab-gpu \
+        -t 168:00:00 \
+        -n 4 \
+        -N 1 \
+        --nodelist=ai-gpu09 \
+        --mem=40G \
+        --gres=gpu:a100:1 \
+        --mail-type=ALL \
+        --mail-user=ras4037@med.cornell.edu \
+        --job-name=$1 \
+        -e ./job_err_EXP/%j-$1.err \
+        -o ./job_out_EXP/%j-$1.out \
+        $2
